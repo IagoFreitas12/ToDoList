65e602cb24c7f92dc996164374a820fb87db394e
IagoFreitas12 <88669758+IagoFreitas12@users.noreply.github.com>
2022-07-11 13:45:19 -0300

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title')
+home
+@endsection
+
+@section('content')
+
+<h1>ToDoList</h1>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    {{-- FontAwesome --}}
+    <script src="https://kit.fontawesome.com/bdc954e40e.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
